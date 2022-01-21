48a9df7fdb21de1534dcbcdd731050c34ee145a2
giwl-21 <muhsin.wahianwar@gmail.com>
2022-01-21 16:15:30 -0500

    Adding functionality shown in lesson
    Made clicking the button do something to the p tags.

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 
 
+$(document).ready(function(){
+    $("#something-btn").click(function(){
+        $("p").text("Bloop!");
+        //$("p").css("color", "green");
+        //$("p").toggle();
+    })
+    
+  
+});   
 
+
+
+
+// function somethingPress(){
+//     var elements = document.getElementsByTagName("p");
+
+//     for (var i = 0; i < elements.length; i++){
+//         console.log(elements[i]);
+//         //elements[i].innerHTML = "Bloop";
+//         //elements[i].style.color = "green";
+//         elements[i].setAttribute("hidden", "true");
+        
+//     }
+// }
 
 // document.addEventListener("DOMContentLoaded", function() {
 //     document.getElementById("something-btn").setAttribute("onclick", "somethingPress()");
